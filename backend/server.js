6b9e9c28db5a9d4866825ee907f380079d54749e
Pawan Nagar <pawannagar2243@gmail.com>
2026-02-17 12:59:36 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-  origin: "https://flower-shop-1-5yi3.onrender.com",
+  origin: "https://flower-shop-1-ji4c.onrender.com"
   methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 
